4d53692aa511b93bc11eb6898278059a267ea583
Daniel Scally <djrscally@gmail.com>
2020-03-07 09:52:52 +0000

    linebreaks

--- astrogator.h
+++ astrogator.h
@@ -35,14 +35,13 @@ struct arg_flags {
 
 // constants
 const char * argp_program_bug_address = "https://github.com/djrscally/astrogator/issues";
-const char * argp_program_version = "\n"
-                                    "astrogator "VERSION" compiled "__DATE__"\n"
+const char * argp_program_version = "astrogator "VERSION" compiled "__DATE__"\n"
                                     "Copyright (C) 2020 Dan Scally\n"
                                     "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
                                     "This is free software: you are free to change and redistribute it.\n"
                                     "There is NO WARRANTY, to the extent permitted by law.\n"
                                     "\n"
-                                    "Written by Dan Scally.\n";
+                                    "Written by Dan Scally.";
 
 // function prototypes for source
 void fix_position(void);
